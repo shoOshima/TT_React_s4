8fb41e5036f57c275f8c95c4202d0d82aa386606
shoOshima <abookmarkmaster@gmail.com>
2023-07-24 23:15:18 +0900

    aho

---
+++ src/Iam.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Iam.css"
+
+export const Iam = () =>{
+  const [lanJp,setLanJp] = useState(1)
+  const [name,setName] = useState('大●　●生')
+  const [old,setOld] = useState(32)
+  const labelArrayStr:(string)[][]=[
+    ['name','old','hobby','日本語へ','The background image is a picture of a frozen runny nose.'],
+    ['名前','年齢','趣味','English','背景は、鼻水が凍った時の写真です。'],
+  ]
+  const [labelText,setLabelText]=useState(labelArrayStr[lanJp]);
+  const hobbyArrayStr:(string)[][]=[
+    ['anime','games','sleep','be lazy'],
+    ['アニメ','ゲーム','寝る','怠惰する'],
+  ]
+  const [hobbyNo,setHobbyNo]=useState(1)
+  const [hobby,setHobby]=useState(hobbyArrayStr[lanJp][hobbyNo]);
+
+  const lanChage = () => {
+    if(lanJp == 0){
+      setLanJp(1);
+      setName('S●o Oh●')
+    }else{
+      setLanJp(0);
+      setName('大●　●生')
+    }
+    setLabelText(labelArrayStr[lanJp])
+    setHobby(hobbyArrayStr[lanJp][hobbyNo])
+  }
+  const hobbyChage = () => {
+    let hNo = hobbyNo +1
+    hNo >=hobbyArrayStr[lanJp].length?hNo=0:''
+    setHobbyNo(hNo)
+    setHobby(hobbyArrayStr[lanJp==0?1:0][hobbyNo])
+  }
+
+  return (
+    <>
+    <div className="IAM">
+    <button className="IAM-Button" onClick={lanChage}>{labelText[3]}</button>
+      <h3>The自己紹介</h3>
+      <p>{labelText[0]} : {name}</p>
+      <button className="IAM-Button" onClick={()=>setOld(old+1)}>+</button><button className="IAM-Button" onClick={()=>setOld(old-1)}>-</button>
+      <p>{labelText[1]} : {old}</p>
+      <button className="IAM-Button" onClick={hobbyChage}>▶</button>
+      <p>{labelText[2]} : {hobby}</p>
+      <p>{labelText[4]} </p>
+    </div>
+    </>
+  );
+}
+
+export default Iam;
